77f1ceb07b40a750f552d79a3b44cc2bca88e31d
Aayanjit <aayan.work99@gmail.com>
2024-05-16 00:16:59 +0530

    Adjusted responsiveness for all screens, added musician data in the carousal, added proper experience pictures and project logos, enhanced ui

--- src/components/landingpage/MembersGallery.tsx
+++ src/components/landingpage/MembersGallery.tsx
@@ -2,15 +2,25 @@ import { musicianGallery } from '../../data/landingPageData'
 
 const MembersGallery = () => {
     return (
-        <div className='w-full pt-20 md:pt-32 pb-20 grid grid-cols-2 md:grid-cols-4 gap-4'>
-            {
-                musicianGallery?.map((data, i) => (
-                    <div key={data?.id} className={`${i === 1 || i === 3 || i === 5 || i === 7 ? '' : 'md:translate-y-16'} group`}>
-                        <img src={data?.image} className='w-full object-cover transition-all duration-500 ease-in-out' alt="" />
-                    </div>
-                ))
-            }
-        </div>
+        <>
+            <div className='w-full pt-20 md:pt-32 pb-20 hidden md:grid grid-cols-4 gap-4'>
+                {
+                    musicianGallery?.map((data, i) => (
+                        <div key={data?.id} className={`${i === 1 || i === 3 || i === 5 || i === 7 ? '' : 'md:translate-y-16'} group`}>
+                            <img src={data?.image} className='w-full object-cover transition-all duration-500 ease-in-out' alt="" />
+                        </div>
+                    ))
+                }
+            </div><div className='w-full pt-20 md:pt-32 pb-20 grid md:hidden grid-cols-2 gap-4'>
+                {
+                    musicianGallery?.slice(0, 4)?.map?.((data, i) => (
+                        <div key={data?.id} className={`${i === 1 || i === 3 || i === 5 || i === 7 ? '' : 'md:translate-y-16'} group`}>
+                            <img src={data?.image} className='w-full object-cover transition-all duration-500 ease-in-out' alt="" />
+                        </div>
+                    ))
+                }
+            </div>
+        </>
     )
 }
 
--- src/data/landingPageData.ts
+++ src/data/landingPageData.ts
@@ -1,9 +1,14 @@
-import aikiyambg from '../assets/images/aikiyam_bg.png'
-import aarohanabg from '../assets/images/arohana_bg.png'
-import third from '../assets/images/03.png'
-import fourth from '../assets/images/04.png'
+import aikiyambg from '../assets/images/experienceCards/01.png'
+import aarohanabg from '../assets/images/experienceCards/02.png'
+import third from '../assets/images/experienceCards/03.png'
+import fourth from '../assets/images/experienceCards/04.png'
 
-import musician from '../assets/images/musician.png'
+import karthik from '../assets/images/musicianCarousal/karthik.png'
+import ramkumar from '../assets/images/musicianCarousal/ramkumar.png'
+import reshwin from '../assets/images/musicianCarousal/reshwin.png'
+import vikram from '../assets/images/musicianCarousal/vikram.png'
+
+import bandVideo from '../assets/videos/bandVideo.webm'
 
 import music_1 from '../assets/images/musicians/01.webp'
 import music_2 from '../assets/images/musicians/02.webp'
@@ -14,8 +19,10 @@ import music_6 from '../assets/images/musicians/06.webp'
 import music_7 from '../assets/images/musicians/07.webp'
 import music_8 from '../assets/images/musicians/08.webp'
 
-import aikiyamLogo from '../assets/icons/aikiyam_logo.png'
-import aarohanaLogo from '../assets/icons/aarohana.svg'
+import aikiyamLogo from '../assets/images/experienceCards/akyam.png'
+import aarohanaLogo from '../assets/images/experienceCards/aarohana.png'
+import ensambleLogo from '../assets/images/experienceCards/ensamble.png'
+import indoSoulLogo from '../assets/images/experienceCards/indoSoul.png'
 
 export const experienceData = [
     {
@@ -34,13 +41,13 @@ export const experienceData = [
         id: 3,
         title: 'Akiyam',
         image: third,
-        logo: aikiyamLogo,
+        logo: indoSoulLogo,
     },
     {
         id: 4,
         title: 'Aarohana',
         image: fourth,
-        logo: aarohanaLogo,
+        logo: ensambleLogo,
     },
 ];
 
@@ -65,11 +72,42 @@ export const ourCareerData = {
     musicians: [
         {
             id: 0,
-            image: musician,
+            image: karthik,
             title: 'KARTHIK IYER',
             about: 'Karthik Iyer, frontman of IndoSoul has been playing the violin since the age of 8. One of the pioneers in the usage of electric violin in the Indian music scene, his vision is to take Indian classical music to a global audience. He has performed across the world in countries like US, Canada, UK, Australia, etc including iconic shows like Later with Jools Holland BBC, Glastonbury festival, MTV Coke Studio to mention a few. He has curated and released four original albums with IndoSoul apart from playing for film music directors like A.R. Rahman, Harris Jayaraj, Anirudh, Imman, etc.',
         },
+        {
+            id: 1,
+            image: reshwin,
+            title: 'RESHWIN NISHITH',
+            about: 'Reshwin has been playing the bass professionally for more than a decade. He has worked with various artists such as Devi Sri Prasad, Chinmayi & Suresh Peters. He is a part of El Fe, the vocal group that performs for artists like A.R. Rahman, Anirudh, D.Imman, and more.',
+        },
+        {
+            id: 2,
+            image: ramkumar,
+            title: 'RAMKUMAR KANAKARAJAN',
+            about: 'Ramkumar has been getting his audience to groove to his beats for over 20 years. He has collaborated with legends like A.R. Rahman, Drums Sivamani and big names like Sid Sriram and Anirudh. He also topped the exams conducted by the Trinity College of Music, London.',
+        },
+        {
+            id: 3,
+            image: karthik,
+            title: 'VENKAT SUBRAMANIAN',
+            about: 'More famously known as Mr. Mridangam in his social circles, hails from a family of artists and started learning Mridangam professionally from when he was thirteen. He specialises not onyl in Mridangam, but also in other precussion instruments like the Morsing, Ghanjeera, Ghatam, Tavil etc. He accompanies as a precussionist and has travelled across India and other countries like Sri Lanka, UAE, Cambodia and Singapore for private concerts. ',
+        },
+        {
+            id: 4,
+            image: vikram,
+            title: 'VIKRAM VIVEKANAND',
+            about: 'Vikram has been in the Chennai music scene for over 15 years now. He has toured extensively with his band Grey Shack, and with La Pongal, Yodhakaa and Junkyard Groove. He has also performed for musicians like Usha Uthup and Shantanu Moitra (MTV Coke Studio) and music directors Harris Jayaraj, K, and Deepak Dev.',
+        },
+        {
+            id: 5,
+            image: karthik,
+            title: 'AKSHAY YESODHARAN',
+            about: 'A trained singer, guitarist and music composer with foundation in Western Classical Piano from the KM Music Conservatory, Akshay Yesodharan has been a professional musician for the past ten years. He has toured with Mr. A.R. Rahman as part ten years. He has toured with Mr. A.R. Rahman as part of his KMMC Chamber Choir, in India as well as Germany for the Bollywood dreams four. His recent works for the South Indian film industry include the background score for a Tamil and Telegu film and vocals for a Tamil web series called The Story of Things.',
+        },
     ],
+    bandVideo: bandVideo,
 };
 
 export const musicianGallery = [
