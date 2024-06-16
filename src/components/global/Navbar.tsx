03e1ac53e550adadb13e50a7aa39862100acd134
Aayanjit <aayan.work99@gmail.com>
2024-06-16 23:34:35 +0530

    Reverted to normal musician gallery from parallax gallery, added buttons to the musicain carousal

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className='w-full py-4 glass-morphism fixed top-0 left-0 z-20 rounded-b-xl'>
+        <div className='hidden md:block w-full py-4 glass-morphism fixed top-0 left-0 z-20 rounded-b-xl'>
             <div className="w-full flex justify-between items-center px-10">
                 <div className='relative'>
                     <h1 className='text-lg text-white font-bold tracking-[0.3em]'>LIVE</h1>
