0f7e17604c80670e42fda38c0f0b872e818f778f
Aayanjit <aayan.work99@gmail.com>
2025-01-04 02:15:38 +0530

    fix: Added fix for all issues preventing app build

--- src/components/soulsabhalandingpage/SeasonHeader.tsx
+++
@@ -1,7 +0,0 @@
-const SeasonHeader = ({ text, styles }: { text: string, styles?: string }) => {
-  return (
-    <h1 className={`gradient-text w-fit font-bebasNeue text-4xl md:text-5xl lg:text-7xl 2xl:text-8xl lowercase mx-auto tracking-wide ${styles}`}>{text}</h1>
-  )
-}
-
-export default SeasonHeader
